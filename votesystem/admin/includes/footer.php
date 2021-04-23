f1ad13b71e38e156faa5250ba7f76f9d5fe63824
Prasitha <prasitha@student.tce.edu>
2021-04-23 16:32:42 +0530

    first commit

---
+++ votesystem/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Online Voting System</b>
+    </div>
+    <strong> &copy; 2020 Brought To You By TCE IT</a></strong>
+</footer>
