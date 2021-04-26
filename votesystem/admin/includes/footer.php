32c25b08d171d4c68edba7340a2bd779474544d4
Prasitha <68117578+Prasitha@users.noreply.github.com>
2021-04-26 23:04:49 +0530

    Delete votesystem/admin directory

--- votesystem/admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Online Voting System</b>
-    </div>
-    <strong> &copy; 2020 Brought To You By TCE IT</a></strong>
-</footer>
